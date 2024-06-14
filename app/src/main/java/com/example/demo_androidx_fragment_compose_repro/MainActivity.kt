5129e5e420bd032287fb49b3a7138cc9f5f03e35
Kingsley Adio <adiksonline@gmail.com>
2024-06-14 23:44:13 +0200

    Repro lost VM state with NavHost and AndroidFragment composable

--- app/src/main/java/com/example/demo_androidx_fragment_compose_repro/MainActivity.kt
+++ app/src/main/java/com/example/demo_androidx_fragment_compose_repro/MainActivity.kt
@@ -1,32 +1,39 @@
 package com.example.demo_androidx_fragment_compose_repro
 
 import android.os.Bundle
-import android.view.ViewGroup
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.get
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
 import androidx.fragment.app.FragmentActivity
-import androidx.navigation.createGraph
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.fragment
-import com.example.demo_androidx_fragment_compose_repro.databinding.ActivityMainBinding
+import androidx.fragment.compose.AndroidFragment
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-class MainActivity : FragmentActivity(R.layout.activity_main) {
-
-    private val binding by lazy(LazyThreadSafetyMode.NONE) {
-        ActivityMainBinding.bind(findViewById<ViewGroup>(android.R.id.content)[0])
-    }
-    private val navController by lazy(LazyThreadSafetyMode.NONE) {
-        binding.container.getFragment<NavHostFragment>().navController
-    }
-
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        navController.apply {
-            graph = createGraph("main") {
-                fragment<MainFragment>("main")
-                fragment<DetailFragment>("detail")
-            }
+        setContent {
+            Main()
         }
+    }
+}
+
+@Composable
+fun Main(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    val view = LocalView.current
+    LaunchedEffect(navController) {
+        Navigation.setViewNavController(view, navController)
+    }
+
+    NavHost(navController, startDestination = "main", modifier = modifier) {
+        composable("main") { AndroidFragment<MainFragment>() }
+        composable("detail") { AndroidFragment<DetailFragment>() }
     }
 }
